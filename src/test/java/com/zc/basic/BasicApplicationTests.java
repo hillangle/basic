58f2a52cb01ef23550c2c1e54e78744dcd4ce68c
仇玮浡 <894636189@qq.com>
2021-09-17 22:26:16 +0800

    基础研发平台

---
+++ src/test/java/com/zc/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
